553d2aa00e747176e1ebeb43cfc6c34e4eff8f2e
Joshua George <georgejw22@hotmail.com>
2024-04-11 17:25:57 -0400

    Upload the initial files

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    public AudioClip clip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(clip, new Vector3(0, 0, 0));
+
+    }
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float Timer = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Timer -= Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) & Timer <=0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Timer = 1f;
+
+
+        }
+    }
+}
